fe6e4c1f2e27112d52889bbcd378dce65d55f82d
jonsag <jonsagebrand@gmail.com>
2021-03-10 16:50:11 +0100

    First commit

---
+++ espScale/espScale.ino
@@ -0,0 +1,4 @@
+//#include "espScaleBasicTest.h"
+//#include "NodeMCU_HX711_ADC.h"
+//#include "NodeMCU_HX711_ADC_Calibration.h"
+#include "NodeMCU_HX711_calibrate.h" // this is the best one of the test sketches
